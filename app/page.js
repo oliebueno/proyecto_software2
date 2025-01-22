8bdd6dc944bbb1d252ecccb7bc42ca7f24b53029
JoseAlfonzo99 <17-10012@usb.ve>
2025-01-22 11:28:48 -0400

    agregado Hola Mundo

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1 className={styles.title}>Hola Mundo</h1>
         <Image
           className={styles.logo}
           src="/next.svg"
